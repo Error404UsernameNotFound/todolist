689d57cccb17f79b4a90e556e9652b409142e0f2
Error404UsernameNotFound <raulmr44@gmail.com>
2022-07-11 05:43:50 +0200

    Pruebas basicas e2e

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,24 @@
+describe('Ejemplo para prueba rápida', () => {
+  it('Visita nuestra app y comprueba cosas', () => {
+    cy.visit('http://localhost:8000/')
+
+    cy.contains("My To Do List")
+
+    cy.contains("Pay bills").click()
+  })
+})
+
+describe('Manejo de tareas', () => {
+  it('Crea una nueva tarea', () => {
+    cy.visit('http://localhost:8000/')
+
+    cy.get('[id=myInput]').type("Regar mis cactuses")
+    cy.get('[class=addBtn]').click()
+
+    cy.contains("Regar mis cactuses")
+  })
+
+  it('Borra la tarea antes creada', () => {
+    cy.get('li').last().children('[class=close]').click()
+  })
+})
